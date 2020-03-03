b72f58681c2ad8e3827a0960784eb04b28052bb2
shortb4483 <61745601+shortb4483@users.noreply.github.com>
2020-03-03 10:06:33 -0500

    first submission

---
+++ P2T1_SalesPrediction_ShortBria.py
@@ -0,0 +1,18 @@
+# This project will captivate the sales prediction of a company
+# Date 2/20/2020
+# CTI-110 P2T1 - Sales Prediction
+# Bria Short
+#
+
+#variables
+totalSales = 0.0
+profit = 0.0
+
+#input
+totalSales = float(input('Enter the projected sales: '))
+
+#calculations
+profit = totalSales * 0.23
+
+#display
+print('The profit is $', format(profit, ',.2f'))
